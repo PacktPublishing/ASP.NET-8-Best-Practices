43d677999777590f978b8fc29aa581493243dc6e
Jonathan <jdanylko@gmail.com>
2023-05-14 10:16:54 -0400

    Update Ch05 Extension classes

--- Ch 5/EFApplication/Extensions/AttractionExtensions.cs
+++ Ch 5/EFApplication/Extensions/AttractionExtensions.cs
@@ -17,15 +17,4 @@ public static class AttractionExtensions
                 : attraction.Location.Name
         };
     }
-}public static class LocationExtensions
-{
-    public static LocationDto ToDto(this Location location)
-    {
-        return new LocationDto
-        {
-            Id = location.Id,
-            Name = location.Name,
-            Attractions = location.Attractions.Select(r=> r.ToDto())
-        };
-    }
 }
---
+++ Ch 5/EFApplication/Extensions/LocationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using EFApplication.DataContext.Models;
+using EFApplication.Dto;
+
+namespace EFApplication.Extensions;
+
+public static class LocationExtensions
+{
+    public static LocationDto ToDto(this Location location)
+    {
+        return new LocationDto
+        {
+            Id = location.Id,
+            Name = location.Name,
+            Attractions = location.Attractions.Select(r=> r.ToDto())
+        };
+    }
+}
