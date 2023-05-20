5c1b7585c15e083f4997d151de71e5749f96d74e
Jonathan <jdanylko@gmail.com>
2023-05-20 14:02:48 -0400

    Create additional tests

---
+++ Ch 7/EFApplication/ThemePark.Tests/Extensions/DateTimeTests.cs
@@ -0,0 +1,77 @@
+using ThemePark.Extensions;
+
+namespace ThemePark.Tests.Extensions;
+
+[TestClass]
+public class DateTimeTests
+{
+    [TestMethod]
+    public void FormattedDateTimeWithTimeTest()
+    {
+        // Arrange
+        var startDate = new DateTime(2023, 5, 2, 14, 0, 0);
+        const string expected = @"May 2<sup>nd</sup>, 2023 at 2:00pm";
+
+        // Act
+        var actual = startDate.ToFormattedDateTime();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void FormattedDateTimeNoTimeTest()
+    {
+        // Arrange
+        var startDate = new DateTime(2023, 5, 2);
+        const string expected = @"May 2<sup>nd</sup>, 2023";
+
+        // Act
+        var actual = startDate.ToFormattedDateTime(false);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void FormattedDateTimeNoTimeFirstDayTest()
+    {
+        // Arrange
+        var startDate = new DateTime(2023, 5, 1);
+        const string expected = @"May 1<sup>st</sup>, 2023";
+
+        // Act
+        var actual = startDate.ToFormattedDateTime(false);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void FormattedDateTimeNoTimeFifthDayTest()
+    {
+        // Arrange
+        var startDate = new DateTime(2023, 5, 5);
+        const string expected = @"May 5<sup>th</sup>, 2023";
+
+        // Act
+        var actual = startDate.ToFormattedDateTime(false);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void FormattedDateTimeNoTimeThirdDayTest()
+    {
+        // Arrange
+        var startDate = new DateTime(2023, 5, 3);
+        const string expected = @"May 3<sup>rd</sup>, 2023";
+
+        // Act
+        var actual = startDate.ToFormattedDateTime(false);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
--- Ch 7/EFApplication/ThemePark.Tests/Pages/PagesTest.cs
+++ Ch 7/EFApplication/ThemePark.Tests/Pages/PagesTest.cs
@@ -1,14 +1,52 @@
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
+using ThemePark.DataContext;
+using ThemePark.DataContext.Models;
+using ThemePark.Dto;
 using ThemePark.Pages;
 using ThemePark.Services;
 
 namespace ThemePark.Tests.Pages;
 
 [TestClass]
-public class PagesTest
+public class PageTests
 {
+    private DbContextOptions<ThemeParkDbContext> _options;
+    private ThemeParkDbContext _context;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _options = new DbContextOptionsBuilder<ThemeParkDbContext>()
+            .UseSqlServer("Data Source=localhost;Initial Catalog=ASPNetCore8BestPractices;"+
+                          "Integrated Security=True;MultipleActiveResultSets=True;"+
+                          "Encrypt=True;TrustServerCertificate=True")
+            .Options;
+
+        var config = new Mock<IConfiguration>();
+
+        _context = new ThemeParkDbContext(_options, config.Object);
+    }
+
+    [TestMethod]
+    [TestCategory("Integration")]
+    public void ReturnAnIndexModelTest()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<IndexModel>>();
+        var service = new AttractionService(_context);
+
+        // Act
+        var actual = new IndexModel(logger.Object, service);
+
+        // Assert
+        Assert.IsNotNull(actual);
+        Assert.IsInstanceOfType(actual, typeof(IndexModel));
+    }
+
     [TestMethod]
     [TestCategory("Integration")]
     public void ConfirmTheMainPageReturnsWithinThreeSecondTest()
@@ -17,16 +55,11 @@ public class PagesTest
         var logger = new Mock<ILogger<IndexModel>>();
         var service = new Mock<IAttractionService>();
 
-        var stopwatch = Stopwatch.StartNew();
-
         // Act
-        _ = new IndexModel(logger.Object, service.Object);
+        var actual = new IndexModel(logger.Object, service.Object);
 
         // Assert
-        // Make sure our call is less than 3 seconds
-        stopwatch.Stop();
-        var duration = stopwatch.Elapsed.Seconds;
-
-        Assert.IsTrue(duration <= 3);
+        Assert.IsNotNull(actual);
+        Assert.IsInstanceOfType(actual, typeof(IndexModel));
     }
 }
